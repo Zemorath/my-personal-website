e8f107a014861baec32fb32ca1dbd6cb11976bb8
Zemorath <trentward10@gmail.com>
2023-04-09 23:18:22 -0500

    restructuring of navigation bar with animation addition

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);*/
 
+const menu = document.getElementById("menu");
+
+Array.from(document.getElementsByClassName("menu-item"))
+    .forEach((item, index) => {
+        item.onmouseover = () => {
+            menu.dataset.activeIndex = index;
+        }
+    })
